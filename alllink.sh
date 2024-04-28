520acab80f9467eeef3f8d1d1c7cc7527eb17568
khakiakhaki <atmosphere.tao@outlook.com>
2024-04-28 20:19:08 +0800

    omtmux-custom
    temp

--- alllink.sh
+++ alllink.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-if [ -d "~/.tmux "];then
-	rm -rf ~/.tmux
-fi
-sudo ln -s ~/.config/tmux ~/.tmux
-
-if [ -d "~/.zshrc"];then
-	rm -rf ~/.zshrc
-fi
-sudo ln -s ~/.congig/.zshrc ~/.zhsrc
+sudo ln -s -f ~/.congig/.zshrc ~/.zhsrc
 
 
 
